d91c8475db37002b90c2449ca69f5aac3e8e1850
Kenny Romanov <kenny.r@trustme.kz>
2025-11-24 15:28:57 +0500

    upd: The file management has been finished; Moving to the flext-asset:// protocol implementation

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,6 @@
-import { NotAFlextFileError } from './errors';
+import { Obj } from '@/types';
+import { BaseError, NotAFlextFileError } from '@/errors';
 import JSZip, { OutputType } from 'jszip';
-// @ts-ignore
 import JSZipSync from 'jszip-sync';
 import Flext from '@trustme24/flext';
 
@@ -12,76 +12,230 @@ import Flext from '@trustme24/flext';
 
 // Types
 
-export type MixedSyncOptions = {
-    sync?: boolean|null,
+export type MixedSyncResult<P extends boolean, T = any> = P extends true ? T : Promise<T>;
+
+export type BundleToFlextFileHandler<P extends boolean, T = any> = (filename: string, type: OutputType) => MixedSyncResult<P, T>;
+
+export type BundleToFlextManifest = {
+    version: string,
+    template: string,
+    assets: Obj<ArrayBuffer>,
 };
 
-export type MixedSyncResult<O extends MixedSyncOptions, T = any> = O['sync'] extends true ? T : Promise<T>;
 
-export type BundleToFlextFileHandler<O extends MixedSyncOptions, T = any> = (filename: string, type: OutputType) => MixedSyncResult<O, T>;
+// Constants
 
-export type BundleToFlextResult<O extends MixedSyncOptions> = MixedSyncResult<O, Flext>;
+export const DEFAULT_VERSION = '1.0.alpha1';
 
 
-// Functions
+// System Functions
 
-// @ts-ignore
-export function bundleToFlext<O extends MixedSyncOptions>(fileHandler: BundleToFlextFileHandler<O>, options: O = {}): BundleToFlextResult<O> {
+export function audit(val: any): string {
+    switch (typeof val) {
+        case 'string':
+            return `'${val}'`;
+        case 'object':
+            return JSON.stringify(val);
+        default:
+            return String(val);
+    }
+}
+
+
+// Checking Functions
+
+export function has(obj: Obj, key: string): boolean {
+    return obj.hasOwnProperty(key);
+}
+
+
+// Framework Functions
+
+export function ensureFilename(val: string): string {
+    let result = val;
+
+
+    // Getting the filename
+
+    if (result.startsWith('/'))
+        result = result.slice(1);
+
+    if (result.endsWith('/'))
+        result = result.slice(0, -1);
+
+
+    return result;
+}
+
+export function strToManifest(val: string, fileHandler: BundleToFlextFileHandler<true>, sync: true): BundleToFlextManifest;
+export function strToManifest(val: string, fileHandler: BundleToFlextFileHandler<false>, sync?: false): Promise<BundleToFlextManifest>;
+export function strToManifest<P extends boolean>(val: string, fileHandler: BundleToFlextFileHandler<P>, sync: P = false as P): MixedSyncResult<P, BundleToFlextManifest> {
+
+    // Getting the data
+
+    let newVal: Obj | null = null;
+
+    try { newVal = JSON.parse(val); }
+    catch (e: any) { throw new BaseError('Flext: Unable to get manifest: ' + e.message); }
+
+
+    // Parsing the data
+
+    const version = String(newVal?.v ?? DEFAULT_VERSION);
+    const templateFilename = newVal?.template ?? null;
+    const assetsObj = newVal?.assets ?? null;
+
+    if (!templateFilename || !assetsObj) throw new NotAFlextFileError();
+
 
     // If the context is sync
 
-    if (options.sync) {
+    if (sync) {
 
-        // Getting the manifest
+        // Getting the template
 
-        const manifest = fileHandler('manifest.json', 'string');
+        const template = fileHandler(ensureFilename(templateFilename), 'string') as string;
 
-        if (!manifest) throw new NotAFlextFileError();
+        if (!template) throw new BaseError('Flext: Unable to get template: ' + audit(templateFilename));
+
+
+        // Getting the assets
+
+        const assets: Obj<ArrayBuffer> = {};
+
+        for (const assetName in assetsObj) {
+            if (!has(assetsObj, assetName)) continue;
+
+            const assetFilename = assetsObj[assetName];
+
+            assets[assetName] = fileHandler(ensureFilename(assetFilename), 'arraybuffer') as ArrayBuffer;
+        }
+
+
+        return {
+            version,
+            template,
+            assets,
+        } as MixedSyncResult<P, BundleToFlextManifest>;
     }
 
 
-    return new Promise((_resolve, reject) => {
+    return new Promise((resolve, reject) => {
+
+        // Making a requests
+
+        const templateRequest = fileHandler(ensureFilename(templateFilename), 'string');
+        const assetNames: string[] = [];
+        const assetFileRequests: Promise<ArrayBuffer>[] = [];
+
+        for (const assetName in assetsObj) {
+            if (!has(assetsObj, assetName)) continue;
+
+
+            // Getting the asset filename
+
+            const assetFilename = assetsObj[assetName];
+
+
+            // Making a request
+
+            assetNames.push(assetName);
+            assetFileRequests.push(fileHandler(ensureFilename(assetFilename), 'arraybuffer'));
+        }
+
+
+        // Parsing the response
+
+        templateRequest.then((template: string) => {
+            Promise.all(assetFileRequests).then((assetFiles: ArrayBuffer[]) => {
+
+                // Getting the assets
+
+                const assets: Obj<ArrayBuffer> = {};
+
+                for (const [ i, file ] of assetFiles.entries()) {
+                    const assetName = assetNames[i] ?? null;
+
+                    if (assetName)
+                        assets[assetName] = file;
+                    else
+                        throw new BaseError('Flext: Unable to get asset: The asset name is out of range');
+                }
+
+
+                resolve({
+                    version,
+                    template,
+                    assets,
+                });
+            }).catch(reject);
+        }).catch(reject);
+    }) as MixedSyncResult<P, BundleToFlextManifest>;
+}
+
+export function bundleToFlext(fileHandler: BundleToFlextFileHandler<true>, sync: true): Flext;
+export function bundleToFlext(fileHandler: BundleToFlextFileHandler<false>, sync?: false): Promise<Flext>;
+export function bundleToFlext<P extends boolean>(fileHandler: BundleToFlextFileHandler<P>, sync: P = false as P): MixedSyncResult<P, Flext> {
+
+    // If the context is sync
+
+    if (sync) {
 
         // Getting the manifest
 
-        fileHandler('manifest.json', 'string').then((manifest) => {
+        const manifestStr = fileHandler('manifest.json', 'string');
+
+        if (!manifestStr) throw new NotAFlextFileError();
+
+
+        // Getting the manifest
+
+        const manifest = strToManifest(manifestStr, fileHandler as BundleToFlextFileHandler<true>, true);
+
+        console.log('_manifest_', manifest);
+
+        return;
+    }
+
+
+    return new Promise((resolve, reject) => {
+
+        // Getting the manifest
+
+        fileHandler('manifest.json', 'string').then((manifestStr) => {
 
             // Doing some checks
 
-            if (!manifest) throw new NotAFlextFileError();
+            if (!manifestStr) throw new NotAFlextFileError();
 
 
-            // Getting the assets
+            // Getting the manifest
 
-            console.log('manifest', manifest);
-
-            const fileRequests = [
-                fileHandler('template.hbs', 'string'),
-            ];
-
-            Promise.all(fileRequests).then((files) => {
-                console.log('assets', files);
-            }).catch(reject);
+            strToManifest(manifestStr, fileHandler as BundleToFlextFileHandler<false>).then(manifest => {
+                console.log('_manifest_', manifest);
+                // @ts-ignore
+                resolve();
+            });
         }).catch(reject);
-    }) as MixedSyncResult<O, Flext>;
+    }) as MixedSyncResult<P, Flext>;
 }
 
 export function getFlextSync(file: ArrayBuffer): Flext {
     const bundle = JSZipSync.load(file);
 
     return bundleToFlext((filename) => {
-        const container = bundle.file(filename);
-        return container.asText();
-    }, { sync: true });
+        const bundleFile = bundle.file(filename);
+        return bundleFile.asText();
+    }, true);
 }
 
 export async function getFlext(file: ArrayBuffer): Promise<Flext> {
     const bundle = await JSZip.loadAsync(file);
 
-    return bundleToFlext(async (filename, type) => {
-        const container = bundle.file(filename);
-        return await container.async(type);
-    }, { sync: false });
+    return await bundleToFlext(async (filename, type) => {
+        const bundleFile = bundle.file(filename);
+        return await bundleFile.async(type);
+    });
 }
 
 // @ts-ignore
